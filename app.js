15f2ddb6558cda608b42b0850e1dfc30a5182ceb
Wogan May <wogan.may@gmail.com>
2017-11-20 16:02:27 +0000

    Refactoring

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const https = require('https');
 const moment = require('moment');
 
 function getLunoTicker(callback) {
+  return getLunoTickerFull(function(ticker) {
+    var dateFormatted = moment(ticker.timestamp);
+    var priceFormatted = parseFloat(ticker.bid).toFixed(0).replace(/./g, function(c, i, a) { return i && c !== "." && ((a.length - i) % 3 === 0) ? ',' + c : c; });
+    callback({
+      text: "`["+dateFormatted+"] BTC/ZAR "+priceFormatted+"`"
+    });
+  });
+}
+
+function getLunoTickerFull(callback)
+{
   return https.get({
     host: 'api.mybitx.com',
     path: '/api/1/ticker?pair=XBTZAR'
@@ -12,12 +23,11 @@ function getLunoTicker(callback) {
     var body = '';
     response.on('data', function(d) { body += d; });
     response.on('end', function() {
+      
       var parsed = JSON.parse(body);
-      var dateFormatted = moment(parsed.ticker.timestamp);
-      var priceFormatted = parseFloat(parsed.ticker.bid).toFixed(0).replace(/./g, function(c, i, a) { return i && c !== "." && ((a.length - i) % 3 === 0) ? ',' + c : c; });
-      callback({
-        text: "`["+dateFormatted+"] BTC/ZAR "+priceFormatted+"`"
-      });
+      
+      callback(parsed);
+      
     });
   });
 }
@@ -43,6 +53,20 @@ client.on("message", async message => {
     });
 
   }
+  
+  if (command === "test1") {
+    
+    getLunoTickerFull(function(ticker){
+      
+      message.channel.send({embed: {
+        color: 3447003,
+        description: "A very simple Embed!"
+      }});
+      
+    });
+    
+  }
+  
 
 });
 
